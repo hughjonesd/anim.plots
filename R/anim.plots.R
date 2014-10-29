1f20c94652f0b0ee21df9c6a3a2106f7a56bbbe6
David Hugh-Jones <davidhughjones@gmail.com>
2014-10-29 10:08:59 +0000

    recycle chunk arguments

--- R/anim.plots.R
+++ R/anim.plots.R
@@ -2,32 +2,21 @@
 #' @import animation
 
 # TODO:
-# - why do you require a matrix? why not just
-# x,y,t and the different values of t define the different plots?
-# this gets rid of all your bullshit impedance mismatch, and allows
-# easy definition of intervals directly from t. It's also what e.g.
-# plot3d does. And interpolation would also be straightforward.
-# presumably "moving window" would also become rather easy.
 #
 # - easy way to annotate an existing plot with points, legend, axes, etc.
-
-# barplot, curve, hist, density, boxplot?, stripchart?
+# curve, hist, density, boxplot?, stripchart?, heatmap, mosaic, contour
+# maps with colours?
+# plot a line being drawn? (more generally, some incremental=TRUE argument?)
+# or a window? http://alstatr.blogspot.co.uk/2014/02/r-animating-2d-and-3d-plots.html
+# perhaps window=1:t gives 'incremental' and window=(t-10):t gives a window?
+# and we can easily just do t %in% window...
+# plot3d - is this possible?
 # generic plot interface? - e.g. plot.density (maybe use xy.coords in .default)
-# "plot moving window" function?
 # generic function interface?
-# interval breaks in the following:
-#    anim.plot(weight ~ chn | Time, data=ChickWeight, subset=chn<5, interval=1, 
-#     col=as.numeric(Diet), smooth=3)
-# 
-# idea:
-# barplot, curve, hist, stripchart, sunflowerplot, matplot..., symbols, arrows, 
-# segments, points, lines, rug, contour, filled.contour, image, rect, polygon
-# text, mtext
-# ... all have basic structure same, just need extensions...
-# * take data and parameters.
-# * interpolate it if necessary.
-# * call a sequence of functions, pausing between each one.
-# * return a list of the calls [or just do this if do.plot=FALSE]
+# smoothing function?
+# make barplot respect matrix parameters; also vectors should go in time order
+# (obscure bug with ncol??)
+# make sure panel.first and panel.last work?
 
 
 .setup.anim <- function () {
@@ -41,10 +30,10 @@
   #if (exists(".old.ani.options")) ani.options(.old.ani.options)
 }
 
-.do.loop <- function(fn, times, show=TRUE, speed=1, use.times=TRUE, slice.args=list(), chop.args=list(),
-  oth.args=list(), arg.dims=list()) {
+.do.loop <- function(fn, times, show=TRUE, speed=1, use.times=TRUE, slice.args=list(), chunk.args=list(),
+  oth.args=list(), arg.dims=list(), chunkargs.ref.length=NULL) {
   # slice.args we take a slice and drop a dimension
-  # chop.args we cut without dropping
+  # chunk.args we cut without dropping
   # oth.args we leave alone
   # individual functions must put things in right boxes
   # examples: matplot() wants matrices so makes sense to pass in 3d arrays
@@ -58,8 +47,10 @@
     }
   }
   
-  for (ar in names(c(slice.args, chop.args))) if (! ar %in% names(arg.dims)) 
+  for (ar in names(slice.args)) if (! ar %in% names(arg.dims)) 
         arg.dims[[ar]] <- 0
+  if (! is.null(chunkargs.ref.length)) for (ar in names(chunk.args)) 
+        chunk.args[[ar]] <- rep(chunk.args[[ar]], chunkargs.ref.length)
   times <- sort(times)
   utimes <- unique(times)
   nframes <- length(utimes)
@@ -77,8 +68,8 @@
             aa[,i], aa[,,i])
     }
     idx <- times==utimes[i]
-    for (cn in names(chop.args)) {
-      ca <- chop.args[[cn]]
+    for (cn in names(chunk.args)) {
+      ca <- chunk.args[[cn]]
       dl <- mydiml(ca)
       args.t[[cn]] <- switch(dl+1, ca, ca[idx], ca[,idx, drop=FALSE])
     }
@@ -115,11 +106,156 @@
   approx(1:size, obj, xout)$y
 }
 
+
+
+#' Create an animated barplot.
+#' 
+#' @param height a vector, matrix or array. If a vector it is divided up by 
+#'   \code{times} and \code{\link{barplot}} is called on each chunk. If a
+#'   matrix, \code{\link{barplot}} is called on each column. If an array, 
+#'   \code{\link{barplot}} is called on each matrix of form \code{height[,,i]}.
+#' @param times a vector of times. If NULL and \code{height} is a matrix,
+#'   the last dimension of \code{height} will be used.
+#' @param show if false, do not show plot; just return calls.
+#' @param speed higher is faster.
+#' @param use.times if \code{TRUE}, animation speed is determined by the
+#' \code{times} argument. If \code{FALSE}, animation speed is constant.
+#' @param width arguments passed to \code{\link{barplot}}. Arguments \code{width, 
+#' names.arg, density, angle, col, border} and \code{offset} may be either vectors
+#' of length \code{length(tbl)} or matrices with one column for each unique value of times.
+#' Other arguments should be length 1 or vectors.
+#' @param space
+#' @param names.arg
+#' @param density
+#' @param angle
+#' @param col
+#' @param border
+#' @param horiz
+#' @param xlim
+#' @param ylim
+#' @param xlab
+#' @param ylab
+#' @param main
+#' @param sub
+#' @param offset
+#' @param legend.text
+#'  
+#' @examples
+#' anim.barplot(1:100, times=rep(1:10, each=10), ylim=c(0,100))
+#' ## barplot with a matrix
+#' ChickWeight$wq <- cut(ChickWeight$weight, 5)
+#' tbl <- as.array(xtabs(~ wq + Diet + Time, data=ChickWeight))
+#' anim.barplot(tbl, xlab="Diet", ylab="N", legend.text=paste("Quintile", 1:5))
+#' anim.barplot(tbl, xlab="Diet", ylab="N", beside=TRUE, ylim=c(0,20),
+#'    legend.text=paste("Quintile", 1:5))
+#'    
+#' @export
+anim.barplot <- function(...) UseMethod("anim.barplot")
+
+#' @export
+anim.barplot.default <- function(height, times=NULL, 
+      show=TRUE, speed=1, use.times=TRUE, width=1, space=NULL, names.arg=NULL, 
+      density=NULL, angle=NULL, col=NULL, border=NULL, horiz=FALSE, xlim=NULL, 
+      ylim=NULL, xlab=NULL, ylab=NULL, main=NULL, sub=NULL, offset=NULL, 
+      legend.text=NULL, ...) {
+  # plot data
+  slice.args <- list(height=height, space=space, xlim=xlim, ylim=ylim, main=main, 
+        sub=sub, xlab=xlab, ylab=ylab, legend.text=legend.text)
+  
+  # in barplot:
+  # height is matrix or vector; space is 1, 2 or length(height); width is length(height)
+  # so is names.arg, density, angle, col, border; legend.text is TRUE or nrow(height)
+  # if height is a matrix and beside = FALSE then we want ncol(height)
+  args <- list(...)
+  chunk.args <- list(width=width, names.arg=names.arg, density=density, 
+        angle=angle, col=col, border=border, offset=offset)
+  
+  ltdim <- if (is.logical(legend.text)) 0 else 1
+  
+  oth.args <- args
+  if (is.vector(height)) chunk.args$height=height else slice.args$height=height
+  hdim <- if(is.matrix(height)) 1 else 2
+  if (is.null(times)) {
+    if (is.array(height)) times <- 1:tail(dim(height), 1) else stop("'times' not specified")
+  }
+
+  arg.dims <- list(height=hdim, space=1, xlim=1, ylim=1, main=0, sub=0, xlab=0, 
+        ylab=0, space=1, legend.text=ltdim)
+  .do.loop(barplot, times=times, use.times=use.times, show=show, speed=speed, 
+        slice.args=slice.args, chunk.args=chunk.args, oth.args=oth.args, 
+        arg.dims=arg.dims)
+}
+
+#' Create an animated plot.
+#' 
+#' \code{anim.plot}
+#' 
+#' @param x,y vectors of x and y values.
+#' @param times a vector of times. If NULL and \code{x} is a matrix, a sequence
+#'   from 1 to the last dimension of \code{x} will be used.
+#' @param show if false, do not show plot; just return calls.
+#' @param speed animation speed.
+#' @param use.times if \code{TRUE}, animation speed is determined by the 
+#'   \code{times} argument. If \code{FALSE}, animation speed is constant.
+#' @param xlim,ylim,col,pch,labels,cex,lty,lwd,asp arguments passed to 
+#'   \code{\link{plot}}.
+#' @param ... Other arguments passed to \code{plot}.
+#'   
+#' @details
+#' 
+#' Each unique level of \code{times} will generate a single frame of animation. 
+#' The frames will be ordered by \code{times}.
+#' 
+#' In general:
+#' 
+#' \itemize{ 
+#' \item Parameters that apply to each point of the plot, such as
+#' \code{xlim, ylim, col, pch, labels} and \code{cex}, can be passed as vectors
+#' which will be recycled to \code{length(times)}. 
+#' \item Parameters that apply
+#' to the plot as a whole, and can have length > 1, such as \code{xlim} and \code{ylim}, can be passed as vectors or matrices. If vectors, the
+#' same vector will be passed to every frame. If matrices, column \code{i} will
+#' be passed to the \code{i}'th frame. 
+#' \item Parameters that apply to the plot
+#' as a whole, and always have length 1, such as \code{xlab} and \code{main},
+#' can be passed as vectors and will be
+#' recycled to the number of frames. 
+#' }
+#' 
+#' @examples
+#' 
+#' x <- rep(1:100/10, 10)
+#' times <- rep(1:10, each=100)
+#' y <- sin(x*times/4)
+#' anim.plot(x,y,times, ylab="Sine wave", type="l")
+#' anim.plot(x,y,times, ylab="Sine wave", type="l", fg="red", col="blue")
+#' ## changing colours - a per-point parameter
+#' cols <- (x+9*times)/100 # length 1000
+#' anim.plot(x,y,times, ylab="Sine wave", type="l", col=rgb(cols, 0, 1-cols), lwd=2)
+#' anim.plot(x,y,times, ylab="Sine wave", type="p", col=rainbow(100)[x *10])
+#' 
+#' ## changing line width - a whole-plot parameter
+#' anim.plot(x, y, lwd=matrix(1:10, ncol=10), type="l")
+#'      
+#' ## discoveries 1860-1959: moving window
+#' dis <- as.vector(sapply(1:91, function(x) discoveries[x:(x+9)]))
+#' years <- outer(0:9, 1860:1951,"+")
+#' anim.plot(years, dis, times=rep(1:100, each=10), xlab="Year", ylab="Discoveries", type="h",
+#'      col="blue", xlim=years[c(1,10),], lwd=8, lab=c(10,5,7))
+#'      
+#' ## Formula interface
+#' ChickWeight$chn <- as.numeric(as.factor(ChickWeight$Chick))
+#' anim.plot(weight ~ chn + Time, data=ChickWeight, col=as.numeric(Diet), 
+#'      pch=as.numeric(Diet), speed=3)
+#' @export
+anim.plot <- function(...) UseMethod("anim.plot")
+
+
 #' @export 
 anim.plot.default <- function (x, y, times, speed=1, use.times=TRUE, xlim=NULL, ylim=NULL, col=par("col"), 
-      pch=par("pch"), cex=1, labels=NULL, asp=NULL, lty=par("lty"), lwd=par("lwd"), 
-    smooth=NULL, ...) {  
-
+  pch=par("pch"), cex=1, labels=NULL, asp=NULL, lty=par("lty"), lwd=par("lwd"), 
+  smooth=NULL, ...) {  
+  
   x <- as.vector(x)
   y <- as.vector(y) # consider doing sthg like xy.coords?
   args <- list(...)
@@ -134,26 +270,26 @@ anim.plot.default <- function (x, y, times, speed=1, use.times=TRUE, xlim=NULL, 
   # col, pch, cex, should be chopped.
   # you could say, if col is a matrix, we apply it once for each frame?
   # labels' length could vary?
-#  mat.args <- list(x=x, y=y, xlim=xlim, ylim=ylim, col=col, pch=pch, cex=cex,
-#  vec.args <- list(asp=asp, lty=lty, lwd=lwd)
-#   labels=labels)
-
-#   if (! is.null(smooth)) {
-#     for (ma in setdiff(names(mat.args), "col")) if (is.matrix(mat.args[[ma]])) 
-#           mat.args[[ma]] <- .interp(mat.args[[ma]], smooth)
-#     for (va in names(vec.args)) if (length(vec.args[[va]]) > 1) vec.args[[va]] <- 
-#           .interp(vec.args[[va]], smooth) 
-#     if (is.matrix(mat.args$col)) mat.args$col <- .col.interp(mat.args$col, smooth)
-#   } 
+  #  mat.args <- list(x=x, y=y, xlim=xlim, ylim=ylim, col=col, pch=pch, cex=cex,
+  #  vec.args <- list(asp=asp, lty=lty, lwd=lwd)
+  #   labels=labels)
+  
+  #   if (! is.null(smooth)) {
+  #     for (ma in setdiff(names(mat.args), "col")) if (is.matrix(mat.args[[ma]])) 
+  #           mat.args[[ma]] <- .interp(mat.args[[ma]], smooth)
+  #     for (va in names(vec.args)) if (length(vec.args[[va]]) > 1) vec.args[[va]] <- 
+  #           .interp(vec.args[[va]], smooth) 
+  #     if (is.matrix(mat.args$col)) mat.args$col <- .col.interp(mat.args$col, smooth)
+  #   } 
   
   
-  chop.args <- list(x=x, y=y, col=col, pch=pch, cex=cex)
+  chunk.args <- list(x=x, y=y, col=col, pch=pch, cex=cex)
   slice.args <- c(list(asp=asp, lty=lty, lwd=lwd), args)
   
   .do.loop(plot, times=times, use.times=use.times, speed=speed, 
-        chop.args=chop.args, slice.args=slice.args, arg.dims=list(
-          xlab=0, ylab=0, xlim=1, ylim=1, lwd=0, lty=0, asp=0,
-          x=1, y=1, col=1, pch=1, cex=1, type=0))
+    chunk.args=chunk.args, slice.args=slice.args, arg.dims=list(
+      xlab=0, ylab=0, xlim=1, ylim=1, lwd=0, lty=0, asp=0,
+      x=1, y=1, col=1, pch=1, cex=1, type=0), chunkargs.ref.length=max(length(x), length(y)))
 }
 
 #' @export 
@@ -191,120 +327,5 @@ anim.plot.formula <- function(x, data=parent.frame(), subset=NULL, na.action=NUL
   # work out matrices for each value of the second part in order
   # do other values come from within data?
 }
-
-
-#' Create an animated barplot.
-#' 
-#' @param height a vector, matrix or array. If a vector it is divided up by 
-#'   \code{times} and \code{\link{barplot}} is called on each chunk. If a
-#'   matrix, \code{\link{barplot}} is called on each column. If an array, 
-#'   \code{\link{barplot}} is called on each matrix of form \code{height[,,i]}.
-#'   
-#' @examples
-#' anim.barplot(1:100, times=rep(1:10, each=10), ylim=c(0,100))
-#' ## barplot with a matrix
-#' ChickWeight$wq <- cut(ChickWeight$weight, 5)
-#' tbl <- as.array(xtabs(~ wq + Diet + Time, data=ChickWeight))
-#' anim.barplot(tbl, xlab="Diet", ylab="N", legend.text=paste("Quintile", 1:5))
-#' anim.barplot(tbl, xlab="Diet", ylab="N", beside=TRUE, ylim=c(0,20),
-#'    legend.text=paste("Quintile", 1:5))
-#'    
-#' @export
-anim.barplot <- function(...) UseMethod("anim.barplot")
-
-#' @export
-anim.barplot.default <- function(height, times=NULL, 
-      show=TRUE, speed=1, use.times=TRUE, width=1, space=NULL, names.arg=NULL, 
-      density=NULL, angle=NULL, col=NULL, border=NULL, horiz=FALSE, xlim=NULL, 
-      ylim=NULL, xlab=NULL, ylab=NULL, main=NULL, sub=NULL, offset=NULL, 
-      legend.text=NULL, ...) {
-  # plot data
-  slice.args <- list(height=height, space=space, xlim=xlim, ylim=ylim, main=main, 
-        sub=sub, xlab=xlab, ylab=ylab, legend.text=legend.text)
-  
-  # in barplot:
-  # height is matrix or vector; space is 1, 2 or length(height); width is length(height)
-  # so is names.arg, density, angle, col, border; legend.text is TRUE or nrow(height)
-  # if height is a matrix and beside = FALSE then we want ncol(height)
-  args <- list(...)
-  chop.args <- list(width=width, names.arg=names.arg, density=density, 
-        angle=angle, col=col, border=border, offset=offset)
-  
-  ltdim <- if (is.logical(legend.text)) 0 else 1
-  
-  oth.args <- args
-  if (is.vector(height)) chop.args$height=height else slice.args$height=height
-  hdim <- if(is.matrix(height)) 1 else 2
-  if (is.null(times)) {
-    if (is.array(height)) times <- 1:tail(dim(height), 1) else stop("'times' not specified")
-  }
-
-  arg.dims <- list(height=hdim, space=1, xlim=1, ylim=1, main=0, sub=0, xlab=0, 
-        ylab=0, space=1, legend.text=ltdim)
-  .do.loop(barplot, times=times, use.times=use.times, show=show, speed=speed, 
-        slice.args=slice.args, chop.args=chop.args, oth.args=oth.args, 
-        arg.dims=arg.dims)
-}
-
-#' Create an animated plot.
-#' 
-#' \code{anim.plot}
-#' 
-#' @param x a matrix or vector. If a matrix, each column is plotted in a single
-#'   frame of the animation. If a vector, the same values are plotted in each
-#'   frame. Either \code{x} or \code{y} must be a matrix.
-#' @param y a matrix or vector.
-#' @param interval how many seconds to wait between each frame.
-#' @param xlim These and subsequent arguments are the same as in
-#'   \code{\link{plot}}.
-#' @param ylim
-#' @param col
-#' @param pch
-#' @param labels
-#' @param cex
-#' @param lty
-#' @param lwd
-#' @param asp
-#' @param smooth Interpolate data by linear smoothing? If NULL, no smoothing is 
-#'   done. If e.g. \code{smooth=2}, the number of plots will be doubled.
-#' @param ... Other arguments passed to \code{plot}.
-#'   
-#' @details Parameters \code{xlim, ylim, col, pch, labels} and \code{cex} can be
-#' matrices. Each column will apply to a single frame of the animation.
-#' 
-#' Parameters \code{asp, lwd} and \code{lty} can be vectors of length > 1, in
-#' which case each value will apply to a single frame.
-#' 
-#' When \code{smooth > 1}, parameters are interpolated where appropriate
-#' (including colours).
-#' 
-#' @examples
-#' 
-#' x <- rep(1:100/10, 10)
-#' times <- rep(1:10, each=100)
-#' y <- sin(x*times/4)
-#' anim.plot(x,y,times, ylab="Sine wave", type="l")
-#' anim.plot(x,y,times, ylab="Sine wave", type="l", fg="red", col="blue")
-#' ## changing colours - a per-point parameter
-#' cols <- (x+9*times)/100 # length 1000
-#' anim.plot(x,y,times, ylab="Sine wave", type="l", col=rgb(cols, 0, 1-cols), lwd=2)
-#' anim.plot(x,y,times, ylab="Sine wave", type="p", col=rainbow(100)[x *10])
-#' 
-#' ## changing line width - a whole-plot parameter
-#' anim.plot(x, y, lwd=matrix(1:10, ncol=10), type="l")
-#'      
-#' ## discoveries 1860-1959: moving window
-#' dis <- as.vector(sapply(1:91, function(x) discoveries[x:(x+9)]))
-#' years <- outer(0:9, 1860:1951,"+")
-#' anim.plot(years, dis, times=rep(1:100, each=10), xlab="Year", ylab="Discoveries", type="h",
-#'      col="blue", xlim=years[c(1,10),], lwd=8, lab=c(10,5,7))
-#'      
-#' ## Formula interface
-#' ChickWeight$chn <- as.numeric(as.factor(ChickWeight$Chick))
-#' anim.plot(weight ~ chn + Time, data=ChickWeight, col=as.numeric(Diet), 
-#'      pch=as.numeric(Diet), speed=3)
-#' @export
-anim.plot <- function(...) UseMethod("anim.plot")
-
 
 
