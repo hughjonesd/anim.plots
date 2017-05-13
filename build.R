9a36f69bdbbbfb12f69df78d2ebd6c6fc6d1a1da
David Hugh-Jones <davidhughjones@gmail.com>
2017-05-13 17:34:32 +0100

    bugfix build script

--- build.R
+++ build.R
@@ -1,11 +1,12 @@
 
-# build script
+# build script, builds vignette manually
 
 # first, create vignette with animation files
 rmarkdown::render('vignettes/anim.plots.Rmd', clean = FALSE)
 # now, vignettes has what we want including the anim.plots_files folder
 # copy them to inst/doc
-# run build with --no-build-vignettes
-file.copy('vignettes/anim.plots.Rmd', 'inst/doc', overwrite = TRUE)
+file.copy('vignettes/anim.plots.html',  'inst/doc', overwrite = TRUE)
+file.copy('vignettes/anim.plots.Rmd',   'inst/doc', overwrite = TRUE)
 file.copy('vignettes/anim.plots_files', 'inst/doc', recursive = TRUE)
+# run build with --no-build-vignettes
 devtools::build(vignettes = FALSE)
