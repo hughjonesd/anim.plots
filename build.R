f78b980d2888fb7d77995a7439c5a33d4b722e94
David Hugh-Jones <davidhughjones@gmail.com>
2017-05-14 08:52:39 +0100

    make build script build website

--- build.R
+++ build.R
@@ -1,12 +1,19 @@
 
-# build script, builds vignette manually
+# build script, builds vignette manually and updates github.io website
+
 
 # first, create vignette with animation files
 rmarkdown::render('vignettes/anim.plots.Rmd', clean = FALSE)
 # now, vignettes has what we want including the anim.plots_files folder
-# copy them to inst/doc
-file.copy('vignettes/anim.plots.html',  'inst/doc', overwrite = TRUE)
-file.copy('vignettes/anim.plots.Rmd',   'inst/doc', overwrite = TRUE)
-file.copy('vignettes/anim.plots_files', 'inst/doc', recursive = TRUE)
+# copy them to inst/doc and to docs for website
+for (dest in c('inst/doc', 'docs')) {
+  file.copy('vignettes/anim.plots.html',  dest, overwrite = TRUE)
+  file.copy('vignettes/anim.plots.Rmd',   dest, overwrite = TRUE)
+  file.copy('vignettes/anim.plots_files', dest, recursive = TRUE)
+}
+# create presentation on website
+rmarkdown::render('docs/anim-plots-presentation.Rmd')
+
+# create presentation
 # run build with --no-build-vignettes
 devtools::build(vignettes = FALSE)
