8dd40db38037a28c07e35ff3952e6ca9799d4bc7
David Hugh-Jones <davidhughjones@gmail.com>
2021-04-30 14:31:38 +0100

    Fix build process to include full vignette as Rmd

--- build.R
+++ build.R
@@ -7,9 +7,6 @@ rmarkdown::render('docs/anim.plots.Rmd', clean = FALSE)
 # create presentation on website
 rmarkdown::render('docs/anim-plots-presentation.Rmd')
 
-# run build. Builds only stub vignette.
-devtools::build() 
-
 # create example for README.md ============================
 setwd('docs')
 x <- rep(rnorm(400), 10)
@@ -17,7 +14,15 @@ y <- rep(rnorm(400), 10)
 xlims <- 4 * 2^-(1:10/10)
 ylims <- xlims <- rbind(xlims, -xlims)
 readme_example <- anim.plot(x, y, times = 10, speed = 2, xlim = xlims, ylim = ylims, col = rgb(0,1,0,.3), pch = 19,
-      show = FALSE)
+                            show = FALSE)
 anim.save(readme_example, filename = 'readme-example.gif')
 setwd('..')
+
+# run build
+file.copy("docs/anim.plots.Rmd", "inst/doc/full/")
+built_pkg <- devtools::build(clean_doc = FALSE)
+
+# release
+devtools::release(pkg = built_pkg)
+
 
